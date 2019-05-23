556c15cf66ef396aaef22102791d5760a93e632e
Joe Mifsud <joe@mifsud.me>
2019-05-23 11:24:51 -0700

    Lint PageInteractive/index.vue

--- ozaria/site/components/interactive/PageInteractive/index.vue
+++ ozaria/site/components/interactive/PageInteractive/index.vue
@@ -1,65 +1,74 @@
 <script>
+  import { getInteractive, getSession } from '../../../api/interactive'
+  import draggableOrderingComponent from './draggableOrdering'
+  import insertCodeComponent from './insertCode'
+  import draggableStatementCompletionComponent from './draggableStatementCompletion'
 
-import { getInteractive, getSession } from '../../../api/interactive'
-import draggableOrderingComponent from './draggableOrdering'
-import insertCodeComponent from './insertCode'
-import draggableStatementCompletionComponent from './draggableStatementCompletion'
+  module.exports = Vue.extend({
+    components: {
+      'draggable-ordering': draggableOrderingComponent,
+      'insert-code': insertCodeComponent,
+      'draggable-statement-completion': draggableStatementCompletionComponent
+    },
 
-module.exports = Vue.extend({
-  props: {
-    interactiveIdOrSlug: {
-      type: String,
-      required: true,
-      default: ''
-    },
-    introLevelId: {
-      type: String,
-      required: true,
-      default: ''
-    },
-    courseInstanceId: {
-      type: String
-    }
-  },
-  data: () => ({
-    interactive: {},
-    interactiveSession: {},
-    interactiveType: ''
-  }),
-  components: {
-    'draggable-ordering': draggableOrderingComponent,
-    'insert-code': insertCodeComponent,
-    'draggable-statement-completion': draggableStatementCompletionComponent
-  },
-  async created() {
-    if (!me.hasInteractiveAccess()) {
-      alert('You must be logged in as an admin to use this page.')
-      return application.router.navigate('/', { trigger: true })
-    }
-    try {
-      this.interactive = await getInteractive(this.interactiveIdOrSlug)
-      this.interactiveType = this.interactive.interactiveType
-      if (!this.interactiveType) {
-        console.error("Interactive type is not set for the interactive", this.interactiveIdOrSlug)
-        noty({ text: 'Interactive type is not set for the interactive', type: 'error', timeout: '2000' })
-        return
+    props: {
+      interactiveIdOrSlug: {
+        type: String,
+        required: true,
+        default: ''
+      },
+
+      introLevelId: {
+        type: String,
+        required: true,
+        default: ''
+      },
+
+      courseInstanceId: {
+        type: String,
+        default: undefined
       }
-      const getSessionOptions = {
-        introLevelId: this.introLevelId,
-        courseInstanceId: this.courseInstanceId
+    },
+
+    data: () => ({
+      interactive: {},
+      interactiveSession: {},
+      interactiveType: ''
+    }),
+
+    async created () {
+      if (!me.hasInteractiveAccess()) {
+        alert('You must be logged in as an admin to use this page.')
+        return application.router.navigate('/', { trigger: true })
       }
-      this.interactiveSession = await getSession(this.interactiveIdOrSlug, getSessionOptions )
-    } catch (err) {
-      console.error("Error:", err)
-      noty({ text: 'Error occured in getting interactives data.', type: 'error', timeout: '2000' })
+
+      try {
+        this.interactive = await getInteractive(this.interactiveIdOrSlug)
+        this.interactiveType = this.interactive.interactiveType
+        if (!this.interactiveType) {
+          console.error('Interactive type is not set for the interactive', this.interactiveIdOrSlug)
+          noty({ text: 'Interactive type is not set for the interactive', type: 'error', timeout: '2000' })
+          return
+        }
+
+        const getSessionOptions = {
+          introLevelId: this.introLevelId,
+          courseInstanceId: this.courseInstanceId
+        }
+
+        this.interactiveSession = await getSession(this.interactiveIdOrSlug, getSessionOptions)
+      } catch (err) {
+        console.error('Error:', err)
+        noty({ text: 'Error occured in getting interactives data.', type: 'error', timeout: '2000' })
+      }
+    },
+
+    methods: {
+      onCompleted () {
+        this.$emit('completed')
+      }
     }
-  },
-  methods: {
-    onCompleted() {
-      this.$emit('completed')
-    }
-  }
-})
+  })
 </script>
 
 <template>
@@ -69,28 +78,29 @@ module.exports = Vue.extend({
     :introLevelId="introLevelId"
     :interactiveSession="interactiveSession"
     :courseInstanceId="courseInstanceId"
-    v-on:completed="onCompleted">
-  </draggable-ordering>
+    @completed="onCompleted"
+  />
+
   <insert-code
     v-else-if="interactiveType == 'insert-code'"
     :interactive="interactive"
     :introLevelId="introLevelId"
     :interactiveSession="interactiveSession"
     :courseInstanceId="courseInstanceId"
-    v-on:completed="onCompleted">
-  </insert-code>
+    @completed="onCompleted"
+  />
+
   <draggable-statement-completion
     v-else-if="interactiveType == 'draggable-statement-completion'"
     :interactive="interactive"
     :introLevelId="introLevelId"
     :interactiveSession="interactiveSession"
     :courseInstanceId="courseInstanceId"
-    v-on:completed="onCompleted">
-  </draggable-statement-completion>
+    v-on:completed="onCompleted"
+  />
 </template>
 
 <style scoped>
 
 </style>
-
 
